4a508e8ba2bd2937bbe9d17f47f74f0a6ec01210
DESKTOP-EUEL2TC\Mehrdad <mehrdad.gang@gmail.com>
2017-04-06 03:27:42 -0700

    Create

---
+++ Assets/Armagedon/Scripts/Interfaces/IShootable.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public delegate void OnDeathDelegate();
+
+public interface IShootable
+{
+    float HP { get; set; }
+
+    float Armor { get; set; }
+
+    List<Reward> Rewards { get; set; }
+
+    event OnDeathDelegate OnDeath; 
+    
+    
+}
+
---
+++ Assets/Armagedon/Scripts/Interfaces/Ishooter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public interface Ishooter  {
+       int CurrentWeaponID { get; set; }
+   
+    List<Weapen> Weapones { get; set; }
+
+   
+   
+    Transform AtackPont();
+
+    bool IsMelee();
+
+
+    void ChooseWepons();
+
+    void Shoot();
+   
+}
+public enum CType
+{
+    Player,
+    Enamey,
+    Frends
+
+};
